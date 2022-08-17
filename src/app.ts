85663eff4bb6117c550826e2f739b9398d12bc32
Michael Manlulu <michael.manlulu@myresman.com>
2022-08-18 04:53:46 +0800

    123. interacting with DOM elements

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,11 @@
 class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
-  element: HTMLFormElement;
+
+  formElement: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
 
   constructor() {
     this.templateElement = document.getElementById(
@@ -13,12 +17,34 @@ class ProjectInput {
       this.templateElement.content,
       true
     );
-    this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.formElement = importedNode.firstElementChild as HTMLFormElement;
+    this.formElement.id = 'user-input';
+
+    this.titleInputElement = this.formElement.querySelector(
+      '#title'
+    ) as HTMLInputElement;
+    this.descriptionInputElement = this.formElement.querySelector(
+      '#description'
+    ) as HTMLInputElement;
+    this.peopleInputElement = this.formElement.querySelector(
+      '#people'
+    ) as HTMLInputElement;
+
+    this.configure();
     this.attach();
   }
 
+  private submitHandler(event: Event) {
+    event.preventDefault();
+    console.log('this.titleInputElement.value::', this.titleInputElement.value);
+  }
+
+  private configure() {
+    this.formElement.addEventListener('submit', this.submitHandler.bind(this));
+  }
+
   private attach() {
-    this.hostElement.insertAdjacentElement('afterbegin', this.element);
+    this.hostElement.insertAdjacentElement('afterbegin', this.formElement);
   }
 }
 
