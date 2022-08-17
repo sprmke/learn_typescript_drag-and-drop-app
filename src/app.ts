cc049d0eec5614bcabfe17a378ad33bc1686704e
Michael Manlulu <michael.manlulu@myresman.com>
2022-08-18 06:56:31 +0800

    127. rendering project lists

--- src/app.ts
+++ src/app.ts
@@ -53,6 +53,41 @@ function Autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
 }
 
 // Classes
+class ProjectList {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  listElement: HTMLElement;
+
+  constructor(private type: 'active' | 'finished') {
+    this.templateElement = document.getElementById(
+      'project-list'
+    )! as HTMLTemplateElement;
+    this.hostElement = document.getElementById('app')! as HTMLDivElement;
+
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
+    this.listElement = importedNode.firstElementChild as HTMLElement;
+    this.listElement.id = `${this.type}-projects`;
+
+    this.attach();
+    this.renderContent();
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement('beforeend', this.listElement);
+  }
+
+  private renderContent() {
+    const listId = `${this.type}-projects-list`;
+    this.listElement.querySelector('ul')!.id = listId;
+    this.listElement.querySelector(
+      'h2'
+    )!.textContent = `${this.type.toUpperCase()} PROJECTS`;
+  }
+}
+
 class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
@@ -148,3 +183,5 @@ class ProjectInput {
 }
 
 const prjInput = new ProjectInput();
+const activePrjList = new ProjectList('active');
+const finishedPrjList = new ProjectList('finished');
